45119e5f16b3686c75d6978b8d323971fea09879
PixelWinner <alex.pixelwinner@gmail.com>
2023-12-05 01:50:29 +0200

    Refactor useDynamicFields hook for better UX
    Refactored the useDynamicFields hook to improve user experience. The changes include removal of useEffects used for syncing field state with Formik's state by using Formik's built-in methods directly. The callbacks have been wrapped in useMemo and useCallback for optimization. The text fields are now rendered directly from the formik values which makes it more performant and reduces chances of uncontrolled input errors.

--- src/utils/hooks/useDynamicFields.hook.tsx
+++ src/utils/hooks/useDynamicFields.hook.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useTranslation } from "react-i18next";
+
+import get from "lodash/get";
 
 import { FormikValues } from "formik";
 
 import MuiCloseIcon from "@mui/icons-material/Close";
 import { Box } from "@mui/material";
 
-import { getFieldError, getIsFieldError } from "@utils/helpers/getFieldError.helper";
+import { getTextFieldProps } from "@utils/helpers/getTextFieldProps.helper";
+import { DynamicField } from "@utils/typings/types/common.types";
 
 import Button from "@components/Button";
 import TextField from "@components/TextField";
@@ -24,109 +27,53 @@ const TextFieldsContainer = styled(Box)`
     gap: 4px;
 `;
 
-type DynamicField = { id: number; label: string; value: string };
-
 type HandleAddField = () => void;
 
-type HandleRemoveField = (id: number) => void;
+type HandleRemoveField = (index: number) => void;
 
-type HandleUpdateField = (id: number, part: string, newValue: unknown) => void;
-
-type RenderTextFields = () => JSX.Element[];
-
-type RenderAddButton = () => JSX.Element;
-
-type useDynamicFieldsReturns = {
+type UseDynamicFieldsReturns = {
     handleAddField: HandleAddField;
-    handleRemoveField: HandleRemoveField;
-    handleUpdateField: HandleUpdateField;
-    dynamicFields: DynamicField[];
-    renderTextFields: RenderTextFields;
-    renderAddButton: RenderAddButton;
+    dynamicTextFields: JSX.Element[];
+    addButton: JSX.Element;
 };
 
-type useDynamicFieldsHook = (formikHook: FormikValues, formikFieldKey: string) => useDynamicFieldsReturns;
-export const useDynamicFields: useDynamicFieldsHook = (formikHook, formikFieldKey) => {
+type UseDynamicFieldsHook = (formikHook: FormikValues, formikFieldKey: string) => UseDynamicFieldsReturns;
+export const useDynamicFields: UseDynamicFieldsHook = (formikHook, formikFieldKey) => {
     const { t } = useTranslation();
-    const [dynamicFields, setDynamicFields] = useState<DynamicField[]>([]);
+    const formikFieldValues: DynamicField[] = get(formikHook.values, formikFieldKey);
 
-    useEffect(() => {
-        //Add initial values to dynamicFields
-        const initialValues = formikHook.values[formikFieldKey] ?? {};
+    const handleAddField: HandleAddField = useCallback(() => {
+        formikHook.setFieldValue(formikFieldKey, [...formikFieldValues, { label: "", value: "" }]);
+    }, [formikFieldValues, formikFieldKey]);
 
-        if (!Object.keys(initialValues).length) {
-            return;
-        }
+    const handleRemoveField: HandleRemoveField = useCallback(
+        (index) => {
+            formikHook.setFieldValue(formikFieldKey, formikFieldValues.toSpliced(index, 1));
+        },
+        [formikFieldValues, formikFieldKey]
+    );
 
-        for (const key in initialValues) {
-            setDynamicFields([...dynamicFields, { id: Math.random(), label: key, value: initialValues[key] }]);
-        }
-    }, []);
+    const dynamicTextFields = useMemo(
+        () =>
+            formikFieldValues?.map((_, index) => {
+                return (
+                    <TextFieldsContainer key={index}>
+                        <TextField {...getTextFieldProps({ formikHook, field: "label", t, pathToValue: getValuePath(formikFieldKey, index, "label") })} />
+                        <TextField {...getTextFieldProps({ formikHook, field: "value", t, pathToValue: getValuePath(formikFieldKey, index, "value") })} />
+                        <CloseIcon onClick={() => handleRemoveField(index)} />
+                    </TextFieldsContainer>
+                );
+            }),
+        [formikHook, formikFieldValues, formikFieldKey]
+    );
 
-    useEffect(() => {
-        //Set dynamicFields in formik value
-        const newValues = dynamicFields.reduce((acc, field) => ({ ...acc, [field.label]: field.value }), {});
-
-        formikHook.setFieldValue(formikFieldKey, newValues);
-    }, [dynamicFields]);
-
-    const handleAddField: HandleAddField = () => {
-        setDynamicFields([...dynamicFields, { id: Math.random(), label: "", value: "" }]);
-    };
-
-    const handleRemoveField: HandleRemoveField = (id) => {
-        setDynamicFields(dynamicFields.filter((field) => field.id !== id));
-    };
-
-    const handleUpdateField: HandleUpdateField = (id, part, newValue) => {
-        setDynamicFields(dynamicFields.map((field) => (field.id === id ? { ...field, [part]: newValue } : field)));
-    };
-
-    const renderTextFields = () => {
-        return dynamicFields.map((field) => {
-            const isError =
-                getIsFieldError({ form: formikHook, key: `properties.${field.label}` }) ||
-                getIsFieldError({ form: formikHook, key: `properties.${field.value}` });
-
-            const helperText =
-                getFieldError({ form: formikHook, key: `properties.${field.label}` }) ?? getFieldError({ form: formikHook, key: `properties.${field.value}` });
-
-            return (
-                <TextFieldsContainer key={field.id}>
-                    <TextField
-                        id={String(field.id)}
-                        type="text"
-                        label={t("general.label")}
-                        error={isError}
-                        helperText={helperText}
-                        value={field.label}
-                        onChange={(e) => handleUpdateField(field.id, "label", e.target.value)}
-                    />
-                    <TextField
-                        id={String(field.id)}
-                        type="text"
-                        label={t("general.value")}
-                        error={isError}
-                        helperText={helperText}
-                        value={field.value}
-                        onChange={(e) => handleUpdateField(field.id, "value", e.target.value)}
-                    />
-                    <CloseIcon onClick={() => handleRemoveField(field.id)} />
-                </TextFieldsContainer>
-            );
-        });
-    };
-
-    const renderAddButton = () => {
-        return <Button onClick={handleAddField}>{t("general.addField")}</Button>;
-    };
+    const addButton = useMemo(() => <Button onClick={handleAddField}>{t("general.addField")}</Button>, [handleAddField]);
 
     return {
         handleAddField,
-        handleRemoveField,
-        handleUpdateField,
-        renderTextFields,
-        renderAddButton,
-        dynamicFields
+        dynamicTextFields,
+        addButton
     };
 };
+
+const getValuePath = (key: string, index: number, value: string) => `${key}.${index}.${value}`;
